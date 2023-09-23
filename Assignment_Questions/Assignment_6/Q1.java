ca8c611282a622532536a2c99e782616cc46b995
javedakhtar2003 <122269037+javedakhtar2003@users.noreply.github.com>
2023-09-23 19:52:15 +0530

    add files

--- Assignment_Questions/Assignment_6/Q1.java
+++ Assignment_Questions/Assignment_6/Q1.java
@@ -1,33 +1,30 @@
 import java.io.Console;
-class Q1{
-    Console c=System.console();
-    float  sale,amo,com;
-    void get_amount()
-    {
-        amo=Integer.parseInt(c.readLine("Enter the Sales amount: "));
+
+class Q1 {
+    Console c = System.console();
+    double sale, amo, com;
+
+    void get_amount() {
+        amo = Integer.parseInt(c.readLine("Enter the Sales amount: "));
     }
 
-    void Commission()
-    {
-        if(amo<=500)
-            com=amo*(5/100);
-            System.out.println(amo*(.05));
-        // if(amo>500 && amo>=2000)
-        //     com=(amo*10/100)+35;
-        // if(amo>2000 && amo<=5000)
-        //     com=(amo*12/100)+185;
-        // if(amo>5000)
-        //     com=amo*13/100;
+    void Commission() {
+        if (amo <= 500)
+            com = amo * 5/100;
+        if (amo > 500 && amo <= 2000)
+            com = amo * 10/100+35;
+        if (amo > 2000 && amo <= 5000)
+            com = amo * 12 / 100 + 185;
+        if (amo > 5000)
+            com = amo * 12.5 / 100;
     }
 
-   void Show_result()
-    {
-        System.out.println("Commission is: "+com);
+    void Show_result() {
+        System.out.println("Commission is: " + com);
     }
 
-    public static void main(String args[])
-    {
-        Q1 ob=new Q1();
+    public static void main(String args[]) {
+        Q1 ob = new Q1();
         ob.get_amount();
         ob.Commission();
         ob.Show_result();
