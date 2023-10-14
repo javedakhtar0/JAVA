820af308f4ddc9153e0b1bdb41943ad7ba3a2e15
Deepak Singh Kushwaha <95338176+DeepakProgrammer1@users.noreply.github.com>
2023-10-14 11:15:26 +0530

    Update Q1.java
    changes in file

--- Assignment_Questions/Assignment_5/Q1.java
+++ Assignment_Questions/Assignment_5/Q1.java
@@ -1,10 +1,2 @@
-class Q1{
-    Q1()        //decraition of default constructor which is called automatically when object created
-    {
-        System.out.println("This is a constructor!");
-    }
-    public static void main(String args[])
-    {
-        Q1 ob=new Q1();
-    }
-}
+#include <stdio.h>
+int main()
