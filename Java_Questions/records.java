625103b24694e57cf7d93b3fbf5d3bae33ac4d85
javedakhtar2003 <122269037+javedakhtar2003@users.noreply.github.com>
2023-10-26 07:17:15 +0530

    chnges

--- Java_Questions/records.java
+++ Java_Questions/records.java
@@ -1,17 +1,32 @@
 import java.io.*;
-class records{
+
+class records {
+    Console c = System.console();
+    String emp_name, dep_name;
+    int emp_id, emp_sal;
+
+    void getdata() {
+        emp_name = c.readLine("Name: ");
+        dep_name = c.readLine("Department: ");
+        emp_id = Integer.parseInt(c.readLine("id: "));
+        emp_sal = Integer.parseInt(c.readLine("Salary: "));
+    }
+
+    void showdata() {
+        System.out.println("Those Employee Record whose salary above the 10000");
+        System.out.println(emp_id);
+        System.out.println(emp_name);
+        System.out.println(dep_name);
+        System.out.println(emp_sal);
+    }
+
     public static void main(String args[])
     {
-        Console c= System.console();
-        String name,month;
-        int year,amount;
-        
-        void getdata()
-        {
-            name=c.readLine("Enter the salesMan_name: ");
-            month=c.readLine("Enter the Month_name: ");
-            year=Integer.parseInt(c.readLine("Enter the year: "));
-            amount=Integer.parseInt(c.readLine("Enter the SAles_Amount: "));
-        }
+        records d[]=new records(6);
+       System.out.println("Enter the Employee Details:");
+       for(int i=0;i<5;i++)
+       {
+            
+       }
     }
 }
